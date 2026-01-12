55ad3ec43c9f66064c70d05225fa982336f80566
unknown <manasshete2006@gmmail.com>
2026-01-12 21:14:33 +0530

    frontend setup

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Notes from "./pages/Notes";
+
+ const isAuthenticated = () => {
+   return !!localStorage.getItem("token");
+ };
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/notes" element={<Notes />} />
+        { <Route
+          path="/notes"
+          element={isAuthenticated() ? <Notes /> : <Navigate to="/login" />}
+        /> }
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        <h1>HAAA mERIII JAANNN  LOGIN KRO</h1>
+        
+    </div>
+  )
+}
+
+export default Login
