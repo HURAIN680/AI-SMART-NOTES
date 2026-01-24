9780c109b7d6c623c20d21635cc14aedb9fc8bf5
unknown <manasshete2006@gmmail.com>
2026-01-24 21:35:30 +0530

    render

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://ai-smart-notes-backend-mvw5.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
